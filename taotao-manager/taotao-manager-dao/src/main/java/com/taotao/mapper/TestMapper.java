704dfdba933ded51644513690a4d4134087f9107
caoz <caozhuang0@126.com>
2019-03-06 21:19:46 +0800

    ssm\dubbo搭建

---
+++ taotao-manager/taotao-manager-dao/src/main/java/com/taotao/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.taotao.mapper;
+
+public interface TestMapper {
+
+	/**
+	 * 从服务器获取当前时间
+	 */
+	public String queryNow();
+}
---
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/TestService.java
@@ -0,0 +1,8 @@
+package com.taotao.service;
+
+public interface TestService {
+/**
+ * 数据库中获取当前时间
+ */
+	public String queryNow();
+}
